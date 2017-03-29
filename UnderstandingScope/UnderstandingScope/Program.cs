7ef1952437505614941648ad05d8db637456e27c
Kevin <kevnaf@gmail.com>
2017-03-28 17:51:51 -0700

    3.28.17.office that was a lot of new info on namespace and class function. This is what I needed

---
+++ UnderstandingScope/UnderstandingScope/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnderstandingScope
+{
+    class Program
+    {
+        private static string k = "";  //DEfined as a field at the class level
+
+        static void Main(string[] args)
+        {
+            string j = "";
+            for (int i = 0; i < 10; i++)
+            {
+                j = i.ToString();
+                k = i.ToString();
+                Console.WriteLine(i);
+
+                if (i == 9)
+                {
+                    string l = i.ToString();
+                }
+                //Console.WriteLine(l); defined within if not accessable in for or any other upper block
+            }
+            Console.WriteLine("Outside of the for: " + j);
+            Console.WriteLine("Outside of the for: " + k);
+
+            HelperMethod();
+
+            Car myCar = new Car();
+            myCar.DoSomething();
+
+            Console.ReadLine();
+        }
+
+        static void HelperMethod()
+        {
+            Console.WriteLine("The value of k from the HelperMethod(): " + k);
+        }
+    }
+
+    class Car
+    {
+        public void DoSomething()
+        {
+            Console.WriteLine(helperMethod());
+        }
+
+        private string helperMethod()
+        {
+            return "Hello World";
+        }
+    }
+}
---
+++ myCodeLibrary/myCodeLibrary/SCrape.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace myCodeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapeWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+        public string  ScrapeWebpage(string url, string filepath)
+        {
+            WebClient client = new WebClient();
+            string reply = client.DownloadString(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
