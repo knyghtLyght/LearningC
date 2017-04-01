b3b34f34a2330a1fd5e922d749d4f8d6aa8a2579
Kevin <kevnaf@gmail.com>
2017-03-31 17:59:45 -0700

    3.31.17.office

---
+++ HandlingExceptions/HandlingExceptions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HandlingExceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string content = File.ReadAllText(@"C:\Users\User\Source\Repos\LearningC\Break.txt");
+                Console.WriteLine(content);
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine("Make sure the name of the file is correcct");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Make sure the path of the file is correcct");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("There was a problem");
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                //Code to finialize
+                //Setting objects to null
+                //Closing database connections
+                Console.WriteLine("closing application...");
+            }
+            Console.ReadLine();
+        }
+    }
+}
